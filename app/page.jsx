ca8234f967601931ef98a4306721dd49d4bfb360
RakibRahman <rakibrahman934@gmail.com>
2024-12-12 12:36:49 +0600

    home page title text changed

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export const metadata = {
   title:
-    "INFINISOFT TECHNOLOGY",
+    "Custom website, mobile app design & development. UIUX & prototyping services",
   description:
     "Custom website, mobile app design & development. UIUX & prototyping services.",
 };
