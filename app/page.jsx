43ddb5e7192f7f9068e884c24d95c0c1fc575d85
rakib <rakib.rahman@trucklagbe.com>
2025-01-01 18:15:39 +0600

    Title changed to a better one, good for seo

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export const metadata = {
   title:
-    "Custom website, mobile app design & development. UIUX & prototyping services",
+    "InfiniSoft Technology |  Professional Websites & App Solutions",
   description:
     "Custom website, mobile app design & development. UIUX & prototyping services.",
 };
