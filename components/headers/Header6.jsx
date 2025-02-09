cfdd63c899efc189481d222f3a25c10a178f3aa2
Rakib <rakibrahman934@gmail.com>
2025-02-09 08:55:19 +0600

    Top header menu button updated

--- components/headers/Header6.jsx
+++ components/headers/Header6.jsx
@@ -52,20 +52,37 @@ export default function Header6({ links }) {
       <div className="inner-nav desktop-nav">
         <ul className="clearlist scroll-nav local-scroll justify-content-end scrollspyLinks">
           <Nav links={links} />
-          <li className="desktop-nav-display">
-            <div className="vr mt-2" />
-          </li>
-
           {/* Book A Free Call Button */}
-          <button
-            onClick={togglePopup}
-            className="btn btn-mod btn-color btn-secondary btn-round btn-hover-anim me-1 mb-xs-10"
-          >
-            <span>Contact Us</span>
-          </button>
+          
         </ul>
       </div>
 
+      {/* Right button */}
+      <div style={{alignContent:"center"}}  className="inner-nav desktop-nav">
+        <ul className="clearlist scroll-nav local-scroll justify-content-end scrollspyLinks">
+          {/* Book A Free Call Button */}
+          <button
+           style={{
+             borderRadius: "50px",
+             height: "2.7rem",
+             minWidth: "12rem",
+             background: "linear-gradient(90deg, #E75778 0%, #8876FF 100%)",
+             border: "none",
+             color: "white", // Ensures text remains visible
+             fontSize: "14px",
+             fontWeight: "bold",
+             boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.15)",
+             cursor: "pointer",
+             // transition: "all 0.3s ease-in-out",       
+           }}
+            onClick={togglePopup}
+            className="btn btn-mod btn-color btn-secondary me-1 mb-xs-10"
+          >
+          HIRE US
+        </button>
+        </ul>
+      </div>
+    
       {/* Popup */}
       <Popup isPopupVisible={isPopupVisible} onClose={togglePopup}>
         <Form togglePopup={togglePopup} />
