71f964769765e7f33cd443bd009b20f6c5094cce
Rakib <rakibrahman934@gmail.com>
2025-04-09 20:37:51 +0600

    about text updated

--- components/homes/home-1/About.jsx
+++ components/homes/home-1/About.jsx
@@ -37,11 +37,11 @@ export default function About() {
       </div>
       <div className="col-lg-6 col-xl-5 offset-xl-1">
         <h4 className="h5">Our Mission</h4>
-        <p className="text-gray">
+        <p className="text-gray section-title-ex-small">
         Our mission at InfiniSoft Technology is to empower businesses by delivering fast, reliable, and tailored digital solutions. We strive to turn visions into reality, creating impactful websites, mobile apps, and user experiences that drive growth and success.
         </p>
         <h4 className="h5">Our Vision</h4>
-        <p className="text-gray">
+        <p className="text-gray section-title-ex-small">
         At InfiniSoft Technology, our vision is to enhance lives and businesses by delivering user-focused digital solutions that solve real-world challenges, drive innovation, and build lasting connections.
         </p>
       </div>
